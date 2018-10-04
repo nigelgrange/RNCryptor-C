df4f0c5c02ad9855f245c3bcc14f90e2bf679d45
= <nigel_grange@hotmail.com>
2018-10-04 09:51:34 +0100

    Working test app

--- JRNCryptor.cpp
+++ JRNCryptor.cpp
@@ -9,28 +9,30 @@ extern "C" {
 JNIEXPORT jbyteArray JNICALL Java_com_nigelgrange_jrncryptor_JRNCryptor_encryptWithPassword
   (JNIEnv *env, jobject obj, jbyteArray dataIn, jstring password) {
   
-    const char* passwordStr = env->GetStringUTFChars(string, 0);
+    const char* passwordStr = env->GetStringUTFChars(password, 0);
 
     int outDataLen = 0;
-        char errbuf[BUFSIZ];
+    char errbuf[BUFSIZ];
+    
+    int dataLen = env->GetArrayLength(dataIn);
+    unsigned char* dataBuf = (unsigned char*)malloc(dataLen);
+    env->GetByteArrayRegion(dataIn, 0, dataLen, reinterpret_cast<jbyte*>(dataBuf));
 
-    
-    const unsigned char* myData = (const unsigned char*)"MYDATA";
-    
-    unsigned char* decrypted = rncryptorc_decrypt_data_with_password(myData, 6, 
+    unsigned char* encrypted = rncryptorc_encrypt_data_with_password(dataBuf, dataLen, 
     RNCRYPTOR3_KDF_ITER,
-    password,
-    strlen(password),
+    passwordStr,
+    strlen(passwordStr),
     &outDataLen,
     errbuf,sizeof(errbuf)-1);
+
+	free(dataBuf);
     
-    printf("NativeDecrypt:%s", errbuf);
 
+	jbyteArray ret = env->NewByteArray(outDataLen);
+	env->SetByteArrayRegion(ret, 0, outDataLen, reinterpret_cast<jbyte*>(encrypted));
 
+	free(encrypted);
 
-	jbyte a[] = {1,2,3,4,5,6};
-    jbyteArray ret = env->NewByteArray(6);
-    env->SetByteArrayRegion (ret, 0, 6, a);
     return ret;
           
   }
@@ -39,9 +41,30 @@ JNIEXPORT jbyteArray JNICALL Java_com_nigelgrange_jrncryptor_JRNCryptor_encryptW
 JNIEXPORT jbyteArray JNICALL Java_com_nigelgrange_jrncryptor_JRNCryptor_decryptWithPassword
   (JNIEnv *env, jobject obj, jbyteArray dataIn, jstring password) {
   
-	jbyte a[] = {6,5,4,3,2,1};
-    jbyteArray ret = env->NewByteArray(6);
-    env->SetByteArrayRegion (ret, 0, 6, a);
+    const char* passwordStr = env->GetStringUTFChars(password, 0);
+
+    int outDataLen = 0;
+    char errbuf[BUFSIZ];
+    
+    int dataLen = env->GetArrayLength(dataIn);
+    unsigned char* dataBuf = (unsigned char*)malloc(dataLen);
+    env->GetByteArrayRegion(dataIn, 0, dataLen, reinterpret_cast<jbyte*>(dataBuf));
+
+    unsigned char* decrypted = rncryptorc_decrypt_data_with_password(dataBuf, dataLen, 
+    RNCRYPTOR3_KDF_ITER,
+    passwordStr,
+    strlen(passwordStr),
+    &outDataLen,
+    errbuf,sizeof(errbuf)-1);
+
+	free(dataBuf);
+    
+
+	jbyteArray ret = env->NewByteArray(outDataLen);
+	env->SetByteArrayRegion(ret, 0, outDataLen, reinterpret_cast<jbyte*>(decrypted));
+
+	free(decrypted);
+	
     return ret;
     
   }
--- TestJC.java
+++ TestJC.java
@@ -8,10 +8,21 @@ public static void main(String[] args) {
 	JRNCryptor jc = new JRNCryptor();
 	
 	byte[] data = new byte[5];
+	for (int i=0; i<5; i++) {
+	data[i] = (byte)i;
+	}
 	
 	byte[] enc = jc.encryptWithPassword(data, "pwd");
 	
 	System.out.println("Enc count = "+enc.length);
+	
+	byte[] dec = jc.decryptWithPassword(enc, "pwd");
+	
+	System.out.println("Dec count = "+dec.length);
+		for (int i=0; i<dec.length; i++) {
+		System.out.println(i+" = "+dec[i]);
+	}
+
 
 }
 
